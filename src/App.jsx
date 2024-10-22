731fabec3e6b370e2e6656735fe5007a9fc5f561
Monir <mrmonir0558@gmail.com>
2024-10-22 12:43:39 +0600

    addeded blog page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
-import './App.css'
+import './App.css';
+import Blogs from './components/Blogs/Blogs';
+import Bookmarks from './components/Bookmarks/Bookmarks';
+import Header from './components/Header/Header';
 
 function App() {
-
-  return (
-      <>
-          <h1 className="text-3xl font-bold underline text-white bg-red-500">Hello world!</h1>
-      </>
-  );
+    return (
+        <>
+            <Header />
+            <div className='md:flex'>
+                <Blogs />
+                <Bookmarks />
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
